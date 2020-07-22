0949886883354ca8635947f57a71f9a82edb960b
dvo19 <dvo1906@gmail.com>
2020-07-23 04:15:36 +0700

    Production

--- Homework01/work01.py
+++ Homework01/work01.py
@@ -1,3 +1,8 @@
-print("Hello!")
-print("GIT")
-print("test")
+# Задача-1: Поработайте с переменными, создайте несколько, выведите на экран,
+# запросите у пользователя несколько чисел и строк и сохраните в переменные, выведите на экран.
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+number = float(input("Enter any number: "))
+
+print(name, age, number)
