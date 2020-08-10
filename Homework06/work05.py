614da316da71794bed43b456f73ee19c5ee40497
dvo19 <dvo1906@gmail.com>
2020-08-11 00:46:34 +0700

    06

---
+++ Homework06/work05.py
@@ -0,0 +1,24 @@
+# Задание-5:
+# Создайте экземпляры классов, передайте значения атрибутов. Выполните доступ к атрибутам, выведите результат.
+# Выполните вызов методов и также покажите результат.
+
+class Employee:
+    emp_count = 0
+
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+        Employee.emp_count += 1
+
+    def display_count(self):
+        print('Всего сотрудников: %d' % Employee.emp_count)
+
+    def display_employee(self):
+        print('Имя: {}. Зарплата: {}'.format(self.name, self.salary))
+
+
+emp1 = Employee("Андрей", 2000)
+emp2 = Employee("Мария", 5000)
+emp1.display_employee()
+emp2.display_employee()
+print("Всего сотрудников: %d" % Employee.emp_count)
